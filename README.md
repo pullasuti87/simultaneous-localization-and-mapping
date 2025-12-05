37e317c3dc074ae219c2489d9e2daf523e7bdc75
tatti@ <pullasutii87@gmail.com>
2025-12-05 11:08:34 +0000

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# SLAM -> Simultaneous Localization and Mapping
+
+## NEEDS
+install GUI features to see video or image
+- sudo apt update
+- sudo apt install -y libgl1 libglib2.0-0
+
+## TODO
+- feature extraction -> identifies points in the image that are trackable 
+- switch form containers to local
+
+### NOTES
+- cv2 (opencv) is computer vision library, contains over 2500 algorithms 
+- using flask is silly...
---
+++ features_extraction.py
@@ -0,0 +1,33 @@
+import cv2
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+video = cv2.VideoCapture('./sample.mp4')
+#  orb (oriented fast and rotated brief) -> finds corners and texture details
+orb = cv2.ORB_create(nfeatures=1000) 
+
+def make_frames():
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        # getting features 
+        points, fingerprint= orb.detectAndCompute(frame, None)
+        frame = cv2.drawKeypoints(frame, points, None, color=(0,255,0), flags=0)
+
+        # encode frame into jpg 
+        ok, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+
+        # no retunr
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/')
+def feed():
+    return Response(make_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
