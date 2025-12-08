2cab8cb21b58d264699146cf3bd67e5bdca5dbbc
pullasuti87 <pullasutii87@gmail.com>
2025-12-08 17:21:55 +0000

    stuck, need help

--- camera_movement.py
+++ camera_movement.py
@@ -1,4 +1,25 @@
 import cv2
 
 
+video = cv2.VideoCapture("sample.mp4")
+
+# draw path on canvas, trajectory Visualization 
+def draw_path(img, v): # v -> vector (x, y, z)
+    pass
+
+
+
 # ???????????
+# ????????????
+
+
+ # NO IDEA........ ASK AI AGAIN 
+
+# while True:
+#     ret, frame = video.read()
+#     if not ret:
+#         break
+# 
+
+video.release()
+cv2.destroyAllWindows()
